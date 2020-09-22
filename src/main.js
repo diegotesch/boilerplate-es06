b172b156ee8890b5ad9dab3384e05c8cf96d511f
diegotesch <diego.tesch@gmail.com>
2020-09-22 00:37:40 -0300

    ajustes api base

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-import axios from "axios";
+import api from "./api";
+
+class App {
+  constructor() {
+    this.registrarEventHandler();
+  }
+
+  registrarEventHandler() {
+    // event handlers here
+  }
+}
+
+new App();
